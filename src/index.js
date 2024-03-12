44c9c1c943e0d822138e810a14b0ac370a1b8e7a
Prashant Bagri <prashantbagri01@gmail.com>
2024-03-12 11:55:05 +0530

    Utility Update - 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import { configDotenv } from "dotenv";
 import ConnectDB from "./db/index.js";
+import app from "./app.js";
 
 configDotenv({
     path: './env'
 })
 
-ConnectDB();
+ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is listening on port ${process.env.PORT}`)
+    })
+    app.on("error", (error)=>{
+        console.log("ERROR occured during connection: ", error)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed.: ", error )
+});
